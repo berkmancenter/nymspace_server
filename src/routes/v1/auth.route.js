ec9d614dfbbd814773fa783c6e7f02eb5d314503
Dylan Wooters <dwooters@gmail.com>
2021-11-30 15:36:08 -0800

    Add swagger documentation for newPseudonym, login, and register end points

--- src/routes/v1/auth.route.js
+++ src/routes/v1/auth.route.js
@@ -6,8 +6,98 @@ const auth = require('../../middlewares/auth');
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Auth
+ *   description: User authentication management
+ */
+
+/**
+ * @swagger
+ * /auth/newPseudonym:
+ *   get:
+ *     description: Returns a random pseudonym and token
+ *     tags: [Auth]
+ *     produces:
+ *      - application/json
+ *     responses:
+ *       200:
+ *         description: pseudonym and token
+ *         content: 
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *                 pseudonym:
+ *                   type: string
+ */
 router.route('/newPseudonym').get(authController.newPseudonym);
+
+/**
+ * @swagger
+ * /auth/register:
+ *   post:
+ *     description: Register a new user
+ *     tags: [Auth]
+ *     produces:
+ *       - application/json
+ *     requestBody:
+ *       required: true
+ *       content: 
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               token:
+ *                 type: string
+ *               pseudonym:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: User object
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               $ref: '#/components/schemas/User'
+ */
 router.post('/register', validate(authValidation.register), authController.register);
+
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     description: Login to the application
+ *     tags: [Auth]
+ *     produces:
+ *       - application/json
+ *     requestBody:
+ *       required: true
+ *       content: 
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: User object
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               $ref: '#/components/schemas/User'
+ */
 router.post('/login', validate(authValidation.login), authController.login);
 router.post('/logout', validate(authValidation.logout), authController.logout);
 router.post('/refresh-tokens', validate(authValidation.refreshTokens), authController.refreshTokens);
