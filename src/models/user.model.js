78fab5be9e784de0a594702ea1b6d0552ed4dfeb
Brendan Miller <code+os@brendanmiller.com>
2024-10-08 17:09:50 -0700

    Fix lint issue with email validation

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -4,7 +4,8 @@ const { roles } = require('../config/roles')
 
 const validateEmail = (email) => {
   const re =
-    /^(([^<>()[]\\.,;:\s@"]+(\.[^<>()[]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    // eslint-disable-next-line
+    /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
   return re.test(email)
 }
 
