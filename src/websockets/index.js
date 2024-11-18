28b1f70317171fa4bf3884d7256e9808d2463e21
Jennifer Hickey <jhickey@cyber.harvard.edu>
2024-11-18 08:34:20 -0500

    Add module for broadcasting events
    Allows server components access to socket.io to broadcast events

--- src/websockets/index.js
+++ src/websockets/index.js
@@ -1,10 +1,11 @@
+/* eslint-disable global-require */
 const http = require('http')
 const { availableParallelism } = require('node:os')
 const cluster = require('node:cluster')
-const { setupMaster, setupWorker } = require('@socket.io/sticky')
-const { createAdapter, setupPrimary } = require('@socket.io/cluster-adapter')
-const logger = require('../config/logger')
+const { setupMaster } = require('@socket.io/sticky')
+const { setupPrimary } = require('@socket.io/cluster-adapter')
 const config = require('../config/config')
+const socketIO = require('./socketIO')
 
 // Initialize an empty worker variable to take
 // the primary cluster instance for the rest of the
@@ -29,33 +30,17 @@ if (cluster.isMaster) {
   }
 } else {
   const httpServer = http.createServer()
-  const io = require('socket.io')(httpServer, {
-    cors: {
-      origin: '*'
-    },
-    connectionStateRecovery: {},
-    // set up the adapter on each worker thread
-    adapter: createAdapter()
-  })
-
-  io.adapter(createAdapter())
-  setupWorker(io)
-
+  socketIO.connect(httpServer)
+  const io = socketIO.connection()
   const registerMessageHandlers = require('./messageHandlers')
   const registerThreadHandlers = require('./threadHandlers')
+  io.addConnectionHandlers([registerMessageHandlers, registerThreadHandlers])
 
-  const onConnection = (socket) => {
-    logger.info('Socket connecting.')
-    registerMessageHandlers(io, socket)
-    registerThreadHandlers(io, socket)
-  }
-
-  io.on('connection', onConnection)
   // receive messages from the rest of the app and pass them on to every
   // child process' socket io instance
   process.on('message', function (message) {
     if (message.thread) {
-      io.in(message.thread).emit(message.event, message.message)
+      socketIO.emit(message.thread, message.event, message.message)
     }
   })
 }
---
+++ src/websockets/socketIO.js
@@ -0,0 +1,56 @@
+const { createAdapter } = require('@socket.io/cluster-adapter')
+const { setupWorker } = require('@socket.io/sticky')
+const logger = require('../config/logger')
+
+let connection = null
+
+class SocketIO {
+  constructor() {
+    this.io = null
+  }
+
+  connect(server) {
+    // eslint-disable-next-line global-require
+    this.io = require('socket.io')(server, {
+      cors: {
+        origin: '*'
+      },
+      connectionStateRecovery: {},
+      // set up the adapter on each worker thread
+      adapter: createAdapter()
+    })
+    this.io.adapter(createAdapter())
+    setupWorker(this.io)
+  }
+
+  emit(channel, event, data) {
+    this.io.in(channel).emit(event, data)
+  }
+
+  addConnectionHandlers(handlers) {
+    this.io.on('connection', (socket) => {
+      logger.info('Socket connecting.')
+      for (const handler of handlers) {
+        handler(this.io, socket)
+      }
+    })
+  }
+
+  static init(server) {
+    if (!connection) {
+      connection = new SocketIO()
+      connection.connect(server)
+    }
+  }
+
+  static getConnection() {
+    if (connection) {
+      return connection
+    }
+  }
+}
+module.exports = {
+  SocketIO,
+  connect: SocketIO.init,
+  connection: SocketIO.getConnection
+}
