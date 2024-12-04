674984568c9a5c26cf53daa47c45cbf9761ff7ec
Brendan Miller <code+os@brendanmiller.com>
2024-12-04 10:44:57 -0800

    Restore correct database connection in poll tests

--- tests/integration/poll.test/variant3.test.js
+++ tests/integration/poll.test/variant3.test.js
@@ -30,11 +30,7 @@ const CHOICE2_TEXT = 'Choice 2'
 describe(`Poll API - Variant 3: ${pollThreeBody.title}`, () => {
   // we keep our database between tests and perform a sequence of tests that should be evaluated in order
   beforeAll(async () => {
-    // TODO: Why is the single test file being run multiple times? This is messing with our DB state
-    // Remove this hack to preserve DB state by making DB name unique when that issue is resolved
-    const uuidStr = new mongoose.Types.ObjectId().toString()
-    const dbUrlForFile = `${config.mongoose.url}-${uuidStr}`
-    await mongoose.connect(dbUrlForFile, config.mongoose.options)
+    await mongoose.connect(config.mongoose.url, config.mongoose.options)
     await Promise.all(Object.values(mongoose.connection.collections).map(async (collection) => collection.deleteMany()))
     await insertUsers([userOne, userTwo, admin, registeredUser])
     await insertTopics([privateTopic])
