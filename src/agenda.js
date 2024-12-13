32ef44d9f59973e266bbdcb4a59e31cb6871f5d5
Jennifer Hickey <jhickey@cyber.harvard.edu>
2024-12-13 08:46:11 -0500

    Add agenda error logging

--- src/agenda.js
+++ src/agenda.js
@@ -1,6 +1,19 @@
 const Agenda = require('agenda')
 const config = require('./config/config')
+const logger = require('./config/logger')
 
 const agenda = new Agenda({ db: { address: config.mongoose.url } })
+
+agenda.on('fail', (err, job) => {
+  logger.error(`[AGENDA] Job ${job.attrs.name} failed:`, {
+    error: err.message,
+    jobData: job.attrs,
+    pid: process.pid
+  })
+})
+
+agenda.on('error', (error) => {
+  logger.error('[AGENDA] Global error:', error)
+})
 
 module.exports = agenda
